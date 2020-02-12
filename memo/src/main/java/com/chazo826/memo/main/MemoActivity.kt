82733222867a00c80807b377981c2e8453f4a7cc
Chazo826 <darker826@gmail.com>
2020-02-13 00:33:53 +0900

    Memo Main Activity 추가

---
+++ memo/src/main/java/com/chazo826/memo/main/MemoActivity.kt
@@ -0,0 +1,13 @@
+package com.chazo826.memo.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chazo826.memo.R
+
+class MemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo)
+    }
+}
