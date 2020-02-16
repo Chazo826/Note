6bff00966eb972900e43dc0b7ca982db6eb1d713
Chazo826 <darker826@gmail.com>
2020-02-16 16:32:32 +0900

    module private 삭제

--- core/src/main/java/com/chazo826/core/dagger/scope/ViewModelKey.kt
+++ core/src/main/java/com/chazo826/core/dagger/scope/ViewModelKey.kt
@@ -6,4 +6,4 @@ import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
