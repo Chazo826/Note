1ca252f36643704d2bc8db78418f9e6dbc2ab1e3
Chazo826 <darker826@gmail.com>
2020-02-18 00:48:31 +0900

    AppDatabase 주입 추가

--- memo/src/main/java/com/chazo826/memo/list/viewmodel/MemoListViewModel.kt
+++ memo/src/main/java/com/chazo826/memo/list/viewmodel/MemoListViewModel.kt
@@ -1,7 +1,11 @@
 package com.chazo826.memo.list.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.chazo826.data.database.AppDatabase
+import javax.inject.Inject
 
-class MemoListViewModel: ViewModel() {
+class MemoListViewModel @Inject constructor(private val appDatabase: AppDatabase): ViewModel() {
+
+
 
 }
