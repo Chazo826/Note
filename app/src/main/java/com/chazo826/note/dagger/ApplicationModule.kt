89f10c6002e5cfe5d90236f96b8297213043341a
Chazo826 <darker826@gmail.com>
2020-02-18 00:47:43 +0900

    데이터 베이스 모듈 추가

--- app/src/main/java/com/chazo826/note/dagger/ApplicationModule.kt
+++ app/src/main/java/com/chazo826/note/dagger/ApplicationModule.kt
@@ -1,8 +1,19 @@
 package com.chazo826.note.dagger
 
+import android.app.Application
+import android.content.Context
+import com.chazo826.data.dagger.DatabaseModule
 import dagger.Module
+import javax.inject.Singleton
 
-@Module(includes = [ViewModelFactoryModule::class])
+@Module(
+    includes = [ViewModelFactoryModule::class,
+        DatabaseModule::class]
+)
 class ApplicationModule {
 
+    @Singleton
+    fun provideContext(application: Application): Context {
+        return application
+    }
 }
