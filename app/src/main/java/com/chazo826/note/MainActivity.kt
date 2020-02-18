c930c1a60264f143c5773867be52200e3e6367df
Chazo826 <darker826@gmail.com>
2020-02-19 02:02:17 +0900

    MemoActivity 로 가도록 연결

--- app/src/main/java/com/chazo826/note/MainActivity.kt
+++ app/src/main/java/com/chazo826/note/MainActivity.kt
@@ -2,11 +2,15 @@ package com.chazo826.note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chazo826.memo.memo.MemoActivityIntents.newIntentForMemoActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        newIntentForMemoActivity().let(::startActivity)
+        finish()
     }
 }
