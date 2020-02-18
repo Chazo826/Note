9253bbbce4613eff62337d6f33f354a0a7839f4d
Chazo826 <darker826@gmail.com>
2020-02-18 18:51:24 +0900

    daggerActivity, fragment import 수정, 데이터바인딩 추가

--- memo/src/main/java/com/chazo826/memo/list/ui/MemoListFragment.kt
+++ memo/src/main/java/com/chazo826/memo/list/ui/MemoListFragment.kt
@@ -1,6 +1,30 @@
 package com.chazo826.memo.list.ui
 
-import dagger.android.support.DaggerFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.chazo826.core.dagger.android.DaggerFragment
+import com.chazo826.memo.R
+import com.chazo826.memo.databinding.FragmentMemoListBinding
 
-class MemoListFragment: DaggerFragment() {
+
+class MemoListFragment : DaggerFragment() {
+
+    private lateinit var binding: FragmentMemoListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_memo_list,
+            container,
+            false
+        )
+        return binding.root
+    }
 }
--- memo/src/main/java/com/chazo826/memo/memo/MemoActivity.kt
+++ memo/src/main/java/com/chazo826/memo/memo/MemoActivity.kt
@@ -1,14 +1,23 @@
 package com.chazo826.memo.memo
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.chazo826.core.dagger.android.DaggerAppCompatActivity
 import com.chazo826.memo.R
-import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_memo.*
 
-class MemoActivity : DaggerAppCompatActivity() {
+class MemoActivity : DaggerAppCompatActivity(R.layout.activity_memo) {
+    private val navController: NavController by lazy { findNavController(R.id.memo_nav_graph) }
+    private val appBarConfiguration: AppBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo)
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
     }
 
 }
