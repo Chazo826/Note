a59f100a6d40a27f272cfb44dc2a2a9f5adf77ac
Chazo826 <darker826@gmail.com>
2020-02-19 02:00:37 +0900

    Theme 추가

--- memo/src/main/java/com/chazo826/memo/memo/MemoActivity.kt
+++ memo/src/main/java/com/chazo826/memo/memo/MemoActivity.kt
@@ -1,23 +1,39 @@
 package com.chazo826.memo.memo
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.chazo826.core.dagger.android.DaggerAppCompatActivity
 import com.chazo826.memo.R
 import kotlinx.android.synthetic.main.activity_memo.*
 
 class MemoActivity : DaggerAppCompatActivity(R.layout.activity_memo) {
-    private val navController: NavController by lazy { findNavController(R.id.memo_nav_graph) }
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
     private val appBarConfiguration: AppBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+        setSupportActionBar(toolbar)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
